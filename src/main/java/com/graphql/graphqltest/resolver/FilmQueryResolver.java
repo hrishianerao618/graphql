92a4858973eca48691692aa1f228364c03e26f87
Hrishikesh Anerao <hrishianerao618@gmail.com>
2023-07-21 23:11:45 +0530

    synchronous graphql call

--- src/main/java/com/graphql/graphqltest/resolver/FilmQueryResolver.java
+++ src/main/java/com/graphql/graphqltest/resolver/FilmQueryResolver.java
@@ -7,17 +7,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Component
 public class FilmQueryResolver implements GraphQLQueryResolver {
+    Logger logger = Logger.getLogger(FilmQueryResolver.class.getName());
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()//instead of hardcoding number of threads
+    );
     @Autowired
     FilmService filmService;
 
-    public List<FilmDTO> getFilms(int pageNo, int size) {
-        return filmService.getALLFilms(pageNo, size);
+    public CompletableFuture<List<FilmDTO>> getFilms(int pageNo, int size) {
+
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    logger.log(Level.INFO, "getFilms Called");
+                    List<FilmDTO> filmDTO = filmService.getALLFilms(pageNo, size);
+                    logger.log(Level.INFO, "getFilms Ended");
+                    return filmDTO;
+                }, executor
+        );
+
     }
 
-    public List<FilmDTO> getFilmsByYear(String year) {
-        return filmService.getFilmsByYear(year);
+    public CompletableFuture<List<FilmDTO>> getFilmsByYear(String year) {
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    logger.log(Level.INFO, "getFilmsByYear Called");
+                    List<FilmDTO> filmDTO = filmService.getFilmsByYear(year);
+                    logger.log(Level.INFO, "getFilmsByYear Ended");
+                    return filmDTO;
+                }, executor
+        );
+
     }
 }
